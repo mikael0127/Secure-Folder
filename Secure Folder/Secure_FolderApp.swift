cb4e6b6c39ea3cae2baeea102c1a2593e08e687f
mikael0127 <101308554+mikael0127@users.noreply.github.com>
2023-04-10 18:04:43 +0800

    Initial Commit

---
+++ Secure Folder/Secure_FolderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Secure_FolderApp.swift
+//  Secure Folder
+//
+//  Created by Mikael Denys Wijaya on 10/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Secure_FolderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
