f116e9e60ee8a8090af1740f84a61b8099662367
mikael0127 <101308554+mikael0127@users.noreply.github.com>
2023-07-02 03:03:17 +0800

    july 2 quick update
    delete old code

--- Secure Folder/FolderManager.swift
+++ Secure Folder/FolderManager.swift
@@ -5,38 +5,7 @@
 //  Created by Mikael Denys Wijaya on 15/06/23.
 //
 //
-//import Foundation
-//
-//class FolderManager {
-//    static func createFolderStructure() {
-//        let fileManager = FileManager.default
-//        let mainFolderURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("MainFolder")
-//
-//        do {
-//            if !fileManager.fileExists(atPath: mainFolderURL.path) {
-//                try fileManager.createDirectory(at: mainFolderURL, withIntermediateDirectories: true, attributes: nil)
-//
-//                let subfolderNames = ["Photos", "Videos", "Documents", "Contacts"]
-//
-//                for name in subfolderNames {
-//                    let subfolderURL = mainFolderURL.appendingPathComponent(name)
-//                    try fileManager.createDirectory(at: subfolderURL, withIntermediateDirectories: true, attributes: nil)
-//                }
-//
-//                print("Folder structure created successfully.")
-//            } else {
-//                print("Folder structure already exists.")
-//            }
-//        } catch {
-//            print("Failed to create folder structure: \(error)")
-//        }
-//    }
-//
-//    static func isDecryptedFolderPresent() -> Bool {
-//        let decryptedFolderPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("MainFolder")
-//        return FileManager.default.fileExists(atPath: decryptedFolderPath.path)
-//    }
-//}
+
 
 import Foundation
 
